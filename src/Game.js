722170e16f6997b22d21a72ae8357ee8701feaf6
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-02-18 23:23:13 +0200

    Add working game

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,39 @@
-import { addAttackListeners, displayTurn, drawIndicators } from "./DOMHandler";
+import {
+  addAttackListeners,
+  displayTurn,
+  drawIndicators,
+  drawPlayerWin,
+  drawComputerWin,
+  initializeDOM,
+} from "./DOMHandler";
+import { Player } from "./Player";
+import { GameBoard } from "./GameBoard";
 
 const Game = () => {
   let turn = undefined;
   let player = undefined;
   let computer = undefined;
+  let winner = undefined;
+  let gameRunning = undefined;
 
-  const initialize = (newPlayer, newComputer) => {
+  const initialize = () => {
+    player = Player();
+    computer = Player();
+    let board1 = GameBoard();
+    let board2 = GameBoard();
+
+    board1.initialize(10, 10, "player");
+    board2.initialize(10, 10, "computer");
+    player.setBoard(board1);
+    computer.setBoard(board2);
+
+    board1.addRandomShips(board1);
+    board2.addRandomShips(board2);
+
     turn = 0; //players turn
-    player = newPlayer;
-    computer = newComputer;
+    gameRunning = true;
+    initializeDOM(player, computer);
+    drawIndicators(player, computer);
     addAttackListeners(player.getBoard(), eventHandler);
     addAttackListeners(computer.getBoard(), eventHandler);
     displayTurn(turn);
@@ -16,39 +41,89 @@ const Game = () => {
 
   const changeTurn = () => {
     if (turn == 0) {
-      // set turn to computer
       turn = 1;
+    } else {
+      turn = 0;
+    }
+    displayTurn(turn);
+  };
 
+  const playTurn = () => {
+    displayTurn(turn);
+    if (turn == 1) {
+      // computer turn
       setTimeout(() => {
         const data = {
           board: player.getBoard(),
           coord: computer.AI.generateAttack(player.getBoard()),
         };
         eventHandler("attack", data);
-      }, 1000);
-    } else {
-      // set turn to player
-      turn = 0;
+      }, 250);
     }
-    displayTurn(turn);
-    drawIndicators(player, computer);
+  };
+
+  const checkForWinner = () => {
+    if (player.getBoard().areShipsSunk()) {
+      return computer;
+    } else if (computer.getBoard().areShipsSunk()) {
+      return player;
+    }
+    return null;
+  };
+
+  const setWinner = (newWinner) => {
+    winner = newWinner;
+    gameRunning = false;
+    if (winner == player) {
+      drawPlayerWin();
+    } else if (winner == computer) {
+      drawComputerWin();
+    }
+  };
+
+  const resetGame = () => {
+    turn = null;
+    player.reset();
+    player = null;
+    computer.reset();
+    computer = null;
+    winner = null;
+    gameRunning = null;
+
+    initialize();
   };
 
   const eventHandler = (event, data) => {
-    if (event === "attack") {
+    if (event === "attack" && gameRunning) {
       if (
         (data.board.getOwner() === "player" && turn === 1) ||
         (data.board.getOwner() === "computer" && turn === 0)
       ) {
-        if (data.board.receiveAttack(data.coord)) {
-          changeTurn();
-          return;
+        const attack = data.board.receiveAttack(data.coord);
+        if (attack !== "fail") {
+          const winner = checkForWinner();
+          if (winner == null) {
+            drawIndicators(player, computer);
+            if (attack == "miss") {
+              changeTurn();
+              playTurn();
+            } else {
+              playTurn();
+            }
+          } else {
+            drawIndicators(player, computer);
+            setWinner(winner);
+          }
         }
       }
     }
   };
 
-  return { initialize, eventHandler };
+  const playAgain = document.getElementById("play-again");
+  playAgain.addEventListener("click", () => {
+    resetGame();
+  });
+  return { initialize, eventHandler, setWinner, resetGame };
 };
 
 export { Game };
--- src/Player.js
+++ src/Player.js
@@ -13,18 +13,55 @@ const Player = () => {
       const boardSize = board.getSize();
       let x = undefined;
       let y = undefined;
+
+      // try attacking next to known hits first
+      const sides = getHitSides(board);
+      for (let i = 0; i < sides.length; i++) {
+        const coord = sides[i];
+        if (
+          !includesArray(board.getHits(), coord) &&
+          !includesArray(board.getMisses(), coord) &&
+          !includesArray(board.getClears(), coord)
+        ) {
+          return coord;
+        }
+      }
+      // then randomize attack
       do {
-        x = Math.floor(Math.random() * boardSize[0]);
-        y = Math.floor(Math.random() * boardSize[1]);
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
       } while (
         includesArray(board.getHits(), [x, y]) ||
-        includesArray(board.getMisses(), [x, y])
+        includesArray(board.getMisses(), [x, y]) ||
+        includesArray(board.getClears(), [x, y])
       );
       return [x, y];
     };
+    const getHitSides = (board) => {
+      let allSides = [];
+
+      const hits = board.getHits();
+      for (let i = 0; i < hits.length; i++) {
+        const hitCoord = hits[i];
+        const p1 = [hitCoord[0] - 1, hitCoord[1]];
+        const p2 = [hitCoord[0] + 1, hitCoord[1]];
+        const p3 = [hitCoord[0], hitCoord[1] - 1];
+        const p4 = [hitCoord[0], hitCoord[1] + 1];
+        const sides = [p1, p2, p3, p4];
+        sides.forEach((p) => {
+          if (board.isWithinBoard(p)) {
+            allSides.push(p);
+          }
+        });
+      }
+      return allSides;
+    };
     return { generateAttack };
   })();
-  return { setBoard, getBoard, AI };
+  const reset = () => {
+    board = null;
+  };
+  return { setBoard, getBoard, AI, reset };
 };
 
 export { Player };
