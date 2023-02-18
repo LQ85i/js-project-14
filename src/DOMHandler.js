61cb0efbfcc84fd1f15b1075eb2e091f5d1bb044
Funxy <jaakko.kakko97@gmail.com>
2023-02-18 23:23:13 +0200

    Add working game

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,5 +1,56 @@
 import { includesArray } from "./functions";
 
+const initializeDOM = (player, computer) => {
+  initializeGrids(player, computer);
+  removeListeners();
+  resetWinMsg();
+};
+
+const initializeGrids = (player, computer) => {
+  let playerGrid = document.querySelector("#board-player .board-grid");
+  let computerGrid = document.querySelector("#board-computer .board-grid");
+  playerGrid.innerHTML = "";
+  computerGrid.innerHTML = "";
+  for (let j = 0; j < 10; j++) {
+    //create row
+    const row = document.createElement("div");
+    row.classList.add("board-row");
+    for (let i = 0; i < 10; i++) {
+      //create cell
+      const cell = document.createElement("div");
+      cell.classList.add("board-cell");
+      row.appendChild(cell);
+    }
+    playerGrid.appendChild(row);
+  }
+  for (let j = 0; j < 10; j++) {
+    //create row
+    const row = document.createElement("div");
+    row.classList.add("board-row");
+    for (let i = 0; i < 10; i++) {
+      //create cell
+      const cell = document.createElement("div");
+      cell.classList.add("board-cell");
+      row.appendChild(cell);
+    }
+    computerGrid.appendChild(row);
+  }
+};
+
+const removeListeners = () => {
+  document.querySelectorAll(".board-cell").forEach((e) => {
+    //
+  });
+};
+
+const resetWinMsg = () => {
+  const board1Grid = document.querySelector("#board-computer .board-grid");
+  const board2Grid = document.querySelector("#board-player .board-grid");
+  board1Grid.classList.remove("blur-out");
+  board2Grid.classList.remove("blur-out");
+  document.querySelectorAll(".win-layer").forEach((e) => e.remove());
+};
+
 const drawIndicators = (player, computer) => {
   document.querySelectorAll(".cell-content").forEach((e) => e.remove());
 
@@ -8,15 +59,18 @@ const drawIndicators = (player, computer) => {
   const playerGrid = playerBoard.querySelector(".board-grid");
   const computerGrid = computerBoard.querySelector(".board-grid");
 
-  for (let j = 0; j < player.getBoard().getSize()[1]; j++) {
+  for (let j = 0; j < 10; j++) {
     const row = playerGrid.children[j];
-    for (let i = 0; i < player.getBoard().getSize()[0]; i++) {
+    for (let i = 0; i < 10; i++) {
       let cell = row.children[i];
       if (player.getBoard().shipInCoordinate([i, j]) !== null) {
         const content = document.createElement("div");
         content.classList.add("cell-content");
         if (includesArray(player.getBoard().getHits(), [i, j])) {
           content.classList.add("indicator-hit");
+          if (player.getBoard().shipInCoordinate([i, j]).isSunk()) {
+            content.classList.add("indicator-sunk");
+          }
         } else {
           content.classList.add("indicator-ship");
         }
@@ -27,19 +81,27 @@ const drawIndicators = (player, computer) => {
           content.classList.add("cell-content");
           content.classList.add("indicator-miss");
           cell.appendChild(content);
+        } else if (includesArray(player.getBoard().getClears(), [i, j])) {
+          const content = document.createElement("div");
+          content.classList.add("cell-content");
+          content.classList.add("indicator-clear");
+          cell.appendChild(content);
         }
       }
     }
   }
-  for (let j = 0; j < computer.getBoard().getSize()[1]; j++) {
+  for (let j = 0; j < 10; j++) {
     const row = computerGrid.children[j];
-    for (let i = 0; i < computer.getBoard().getSize()[0]; i++) {
+    for (let i = 0; i < 10; i++) {
       let cell = row.children[i];
       if (computer.getBoard().shipInCoordinate([i, j]) !== null) {
         const content = document.createElement("div");
         content.classList.add("cell-content");
         if (includesArray(computer.getBoard().getHits(), [i, j])) {
           content.classList.add("indicator-hit");
+          if (computer.getBoard().shipInCoordinate([i, j]).isSunk()) {
+            content.classList.add("indicator-sunk");
+          }
         }
         cell.appendChild(content);
       } else {
@@ -48,6 +110,11 @@ const drawIndicators = (player, computer) => {
           content.classList.add("cell-content");
           content.classList.add("indicator-miss");
           cell.appendChild(content);
+        } else if (includesArray(computer.getBoard().getClears(), [i, j])) {
+          const content = document.createElement("div");
+          content.classList.add("cell-content");
+          content.classList.add("indicator-clear");
+          cell.appendChild(content);
         }
       }
     }
@@ -55,8 +122,8 @@ const drawIndicators = (player, computer) => {
 };
 
 const addAttackListeners = (board, eventHandler) => {
-  let boardElem = undefined;
-  let gridElem = undefined;
+  let boardElem = null;
+  let gridElem = null;
 
   if (board.getOwner() == "player") {
     boardElem = document.getElementById("board-player");
@@ -65,12 +132,17 @@ const addAttackListeners = (board, eventHandler) => {
     boardElem = document.getElementById("board-computer");
     gridElem = boardElem.querySelector(".board-grid");
   }
-  for (let j = 0; j < board.getSize()[1]; j++) {
+  for (let j = 0; j < 10; j++) {
     const row = gridElem.children[j];
-    for (let i = 0; i < board.getSize()[0]; i++) {
+    for (let i = 0; i < 10; i++) {
       let cell = row.children[i];
+
       cell.addEventListener("click", () => {
-        eventAttack(board, [i, j], eventHandler);
+        const data = {
+          board,
+          coord: [i, j],
+        };
+        eventHandler("attack", data);
       });
     }
   }
@@ -88,9 +160,35 @@ const displayTurn = (turn) => {
   }
 };
 
-const eventAttack = (board, coord, eventHandler) => {
-  const data = { board, coord };
-  eventHandler("attack", data);
+const drawComputerWin = () => {
+  const board = document.getElementById("board-player");
+  const board1Grid = document.querySelector("#board-computer .board-grid");
+  const board2Grid = document.querySelector("#board-player .board-grid");
+  board1Grid.classList.add("blur-out");
+  board2Grid.classList.add("blur-out");
+  const winLayer = document.createElement("div");
+  winLayer.classList.add("win-layer");
+  winLayer.innerHTML = "Computer Wins!";
+  board.appendChild(winLayer);
 };
 
-export { drawIndicators, addAttackListeners, displayTurn };
+const drawPlayerWin = () => {
+  const board = document.getElementById("board-computer");
+  const board1Grid = document.querySelector("#board-computer .board-grid");
+  const board2Grid = document.querySelector("#board-player .board-grid");
+  board1Grid.classList.add("blur-out");
+  board2Grid.classList.add("blur-out");
+  const winLayer = document.createElement("div");
+  winLayer.classList.add("win-layer");
+  winLayer.innerHTML = "Player Wins!";
+  board.appendChild(winLayer);
+};
+
+export {
+  drawIndicators,
+  addAttackListeners,
+  displayTurn,
+  drawPlayerWin,
+  drawComputerWin,
+  initializeDOM,
+};
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,4 +1,5 @@
 import { includesArray } from "./functions";
+import { Ship } from "./Ship";
 
 const GameBoard = () => {
   let width = undefined;
@@ -8,6 +9,7 @@ const GameBoard = () => {
   let hits = undefined;
   let misses = undefined;
   let owner = undefined;
+  let clears = undefined;
   const initialize = (w, h, newOwner) => {
     if (w >= 1 && h >= 1) {
       width = w;
@@ -19,6 +21,7 @@ const GameBoard = () => {
       hits = [];
       misses = [];
       ships = [];
+      clears = [];
       owner = newOwner;
     }
   };
@@ -41,23 +44,82 @@ const GameBoard = () => {
     }
   };
 
+  const addDefaultShips = (board) => {
+    const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+    for (let i = 0; i < shipLengths.length; i++) {
+      let ship = Ship();
+      ship.setLength(shipLengths[i]);
+      if (i < 5) {
+        board.addShip(ship, [i, i * 2, 0]);
+      } else {
+        board.addShip(ship, [i, (i - 5) * 2, 0]);
+      }
+    }
+  };
+
+  const addRandomShips = (board) => {
+    const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+    let x = undefined;
+    let y = undefined;
+    let dir = undefined;
+    for (let i = 0; i < shipLengths.length; i++) {
+      do {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        dir = Math.floor(Math.random() * 2);
+      } while (!board.shipPositionAvailable(shipLengths[i], [x, y, dir]));
+      let ship = Ship();
+      ship.setLength(shipLengths[i]);
+      board.addShip(ship, [x, y, dir]);
+    }
+  };
+
   const shipPositionAvailable = (length, pos) => {
     if (pos[2] === 0) {
       for (let i = 0; i < length; i++) {
         const coord = [pos[0] + i, pos[1]];
-        if (shipInCoordinate(coord) !== null || !_isWithinBoard(coord)) {
+        if (!isWithinBoard(coord)) {
           return false;
+        } else {
+          if (shipInCoordinate(coord) !== null || isNextToShip(coord)) {
+            return false;
+          }
         }
       }
     } else if (pos[2] === 1) {
       for (let i = 0; i < length; i++) {
         const coord = [pos[0], pos[1] + i];
-        if (shipInCoordinate(coord) !== null || !_isWithinBoard(coord)) {
+        if (!isWithinBoard(coord)) {
           return false;
+        } else {
+          if (shipInCoordinate(coord) !== null || isNextToShip(coord)) {
+            return false;
+          }
         }
       }
     }
     return true;
+  };
+
+  const isNextToShip = (coord) => {
+    const p1 = [coord[0] - 1, coord[1] - 1];
+    const p2 = [coord[0] - 1, coord[1]];
+    const p3 = [coord[0] - 1, coord[1] + 1];
+    const p4 = [coord[0], coord[1] - 1];
+    const p5 = [coord[0], coord[1] + 1];
+    const p6 = [coord[0] + 1, coord[1] - 1];
+    const p7 = [coord[0] + 1, coord[1]];
+    const p8 = [coord[0] + 1, coord[1] + 1];
+    const adjacents = [p1, p2, p3, p4, p5, p6, p7, p8];
+    for (let i = 0; i < adjacents.length; i++) {
+      const p = adjacents[i];
+      if (isWithinBoard(p)) {
+        if (shipInCoordinate(p) !== null) {
+          return true;
+        }
+      }
+    }
+    return false;
   };
 
   const shipInCoordinate = (coord) => {
@@ -80,10 +142,13 @@ const GameBoard = () => {
   const getMisses = () => {
     return misses;
   };
+  const getClears = () => {
+    return clears;
+  };
   const getOwner = () => {
     return owner;
   };
-  const _isWithinBoard = (coord) => {
+  const isWithinBoard = (coord) => {
     if (
       coord[0] >= 0 &&
       coord[0] < width &&
@@ -95,17 +160,26 @@ const GameBoard = () => {
     return false;
   };
   const receiveAttack = (coord) => {
-    if (!includesArray(hits, coord) && !includesArray(misses, coord)) {
+    if (
+      !includesArray(hits, coord) &&
+      !includesArray(misses, coord) &&
+      !includesArray(clears, coord)
+    ) {
       const ship = shipInCoordinate(coord);
       if (ship !== null) {
         ship.hit();
         hits.push(coord);
+        markCornersClear(coord);
+        if (ship.isSunk()) {
+          markSurroundingsClear(ship);
+        }
+        return "hit";
       } else {
         misses.push(coord);
+        return "miss";
       }
-      return true;
     }
-    return false;
+    return "fail";
   };
   const areShipsSunk = () => {
     for (let i = 0; i < ships.length; i++) {
@@ -116,19 +190,82 @@ const GameBoard = () => {
     }
     return true;
   };
+  const markSurroundingsClear = (ship) => {
+    const pos = ship.getPosition();
+    for (
+      let x = pos[0] - 1;
+      x <= pos[0] + Math.max(1, Math.abs(pos[2] - 1) * ship.getLength());
+      x++
+    ) {
+      if (x < 0 || x > 9) {
+        continue;
+      }
+
+      for (
+        let y = pos[1] - 1;
+        y <= pos[1] + Math.max(1, pos[2] * ship.getLength());
+        y++
+      ) {
+        if (y < 0 || y > 9) {
+          continue;
+        }
+        if (
+          !includesArray(misses, [x, y]) &&
+          !includesArray(hits, [x, y]) &&
+          !includesArray(clears, [x, y]) &&
+          !shipInCoordinate([x, y])
+        ) {
+          clears.push([x, y]);
+        }
+      }
+    }
+  };
+
+  const markCornersClear = (hitCoord) => {
+    const p1 = [hitCoord[0] - 1, hitCoord[1] - 1];
+    const p2 = [hitCoord[0] + 1, hitCoord[1] - 1];
+    const p3 = [hitCoord[0] - 1, hitCoord[1] + 1];
+    const p4 = [hitCoord[0] + 1, hitCoord[1] + 1];
+    const corners = [p1, p2, p3, p4];
+    corners.forEach((p) => {
+      if (
+        isWithinBoard(p) &&
+        !includesArray(misses, p) &&
+        !includesArray(clears, p)
+      ) {
+        clears.push(p);
+      }
+    });
+  };
+
+  const reset = () => {
+    width = null;
+    height = null;
+    ships = null;
+    map = null;
+    hits = null;
+    misses = null;
+    owner = null;
+  };
 
   return {
     initialize,
     addShip,
+    addDefaultShips,
+    addRandomShips,
     shipInCoordinate,
+    shipPositionAvailable,
+    isWithinBoard,
     getSize,
     getShips,
     getMap,
     getHits,
     getMisses,
+    getClears,
     getOwner,
     receiveAttack,
     areShipsSunk,
+    reset,
   };
 };
 
